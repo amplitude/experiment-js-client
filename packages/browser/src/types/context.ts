c7eb9a011096c97bc5a345a35e623c226c292a96
Brian Giori <brian.giori@amplitude.com>
2021-05-25 20:39:42 -0700

    feat: update ContextProvider to return an object

--- packages/browser/src/types/context.ts
+++ packages/browser/src/types/context.ts
@@ -1,9 +1,3 @@
-/**
- * A ContextProvider injects information into the {@link ExperimentUser} object
- * before sending a request to the server. This can be used to pass
- * identity (deviceId and userId), or other platform specific context.
- * @category Context Provider
- */
 export interface ExperimentContext {
   deviceId?: string;
   userId?: string;
@@ -14,4 +8,10 @@ export interface ExperimentContext {
   deviceModel?: string;
 }
 
+/**
+ * A ContextProvider injects information into the {@link ExperimentUser} object
+ * before sending a request to the server. This can be used to pass
+ * identity (deviceId and userId), or other platform specific context.
+ * @category Context Provider
+ */
 export type ContextProvider = () => ExperimentContext;
