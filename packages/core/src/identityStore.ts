ace689b83f3cd4f7b0ca5d380ddf7778890f36e7
Brian Giori <brian.giori@amplitude.com>
2021-11-22 11:43:40 -0800

    complete tests

--- packages/core/src/identityStore.ts
+++ packages/core/src/identityStore.ts
@@ -39,7 +39,7 @@ export class IdentityStoreImpl implements IdentityStore {
   editIdentity(): IdentityEditor {
     // eslint-disable-next-line @typescript-eslint/no-this-alias
     const self: IdentityStore = this;
-    const actingIdentity: Identity = Object.assign(this.identity);
+    const actingIdentity: Identity = { ...this.identity };
     const editor: IdentityEditor = {
       setUserId: function (userId: string): IdentityEditor {
         actingIdentity.userId = userId;
@@ -76,14 +76,14 @@ export class IdentityStoreImpl implements IdentityStore {
               break;
             case ID_OP_SET_ONCE:
               for (const [key, value] of Object.entries(properties)) {
-                if (!actingIdentity.userProperties[key]) {
+                if (!actingProperties[key]) {
                   actingProperties[key] = value;
                 }
               }
               break;
             case ID_OP_ADD:
               for (const [key, value] of Object.entries(properties)) {
-                const actingValue = actingProperties[key];
+                const actingValue = actingProperties[key] ?? 0;
                 if (
                   typeof actingValue === 'number' &&
                   typeof value === 'number'
@@ -94,17 +94,17 @@ export class IdentityStoreImpl implements IdentityStore {
               break;
             case ID_OP_APPEND:
               for (const [key, value] of Object.entries(properties)) {
-                const actingValue = actingProperties[key];
+                const actingValue = actingProperties[key] ?? [];
                 if (Array.isArray(actingValue) && Array.isArray(value)) {
-                  actingProperties[key] = actingValue.push(...value);
+                  actingProperties[key] = actingValue.concat(value);
                 }
               }
               break;
             case ID_OP_PREPEND:
               for (const [key, value] of Object.entries(properties)) {
-                const actingValue = actingProperties[key];
+                const actingValue = actingProperties[key] ?? [];
                 if (Array.isArray(actingValue) && Array.isArray(value)) {
-                  actingProperties[key] = value.push(...actingValue);
+                  actingProperties[key] = value.concat(actingValue);
                 }
               }
               break;
@@ -126,13 +126,13 @@ export class IdentityStoreImpl implements IdentityStore {
   }
 
   getIdentity(): Identity {
-    return Object.assign(this.identity);
+    return { ...this.identity };
   }
 
   setIdentity(identity: Identity): void {
-    const originalIdentity = Object.assign(this.identity);
-    this.identity = Object.assign(identity);
-    if (originalIdentity != identity) {
+    const originalIdentity = { ...this.identity };
+    this.identity = { ...identity };
+    if (!isEqual(originalIdentity, this.identity)) {
       this.listeners.forEach((listener) => {
         listener(identity);
       });
@@ -147,3 +147,50 @@ export class IdentityStoreImpl implements IdentityStore {
     this.listeners.delete(listener);
   }
 }
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const isEqual = (obj1: any, obj2: any): boolean => {
+  const primitive = ['string', 'number', 'boolean', 'undefined'];
+  const typeA = typeof obj1;
+  const typeB = typeof obj2;
+  if (typeA !== typeB) {
+    return false;
+  }
+  if (primitive.includes(typeA)) {
+    return obj1 === obj2;
+  }
+  //if got here - objects
+  if (obj1.length !== obj2.length) {
+    return false;
+  }
+  //check if arrays
+  const isArrayA = Array.isArray(obj1);
+  const isArrayB = Array.isArray(obj2);
+  if (isArrayA !== isArrayB) {
+    return false;
+  }
+  if (isArrayA && isArrayB) {
+    //arrays
+    for (let i = 0; i < obj1.length; i++) {
+      if (!isEqual(obj1[i], obj2[i])) {
+        return false;
+      }
+    }
+  } else {
+    //objects
+    const sorted1 = Object.keys(obj1).sort();
+    const sorted2 = Object.keys(obj2).sort();
+    if (!isEqual(sorted1, sorted2)) {
+      return false;
+    }
+    //compare object values
+    let result = true;
+    Object.keys(obj1).forEach((key) => {
+      if (!isEqual(obj1[key], obj2[key])) {
+        result = false;
+      }
+    });
+    return result;
+  }
+  return true;
+};
--- packages/core/test/identityStore.test.ts
+++ packages/core/test/identityStore.test.ts
@@ -1,4 +1,6 @@
 /* eslint-disable no-console */
+import amplitude from 'amplitude-js';
+
 import { IdentityStoreImpl } from '../src/identityStore';
 
 test('editIdentity, setUserId setDeviceId, success', async () => {
@@ -12,9 +14,203 @@ test('editIdentity, setUserId setDeviceId, success', async () => {
   expect(identity).toEqual({ userId: 'user_id', deviceId: 'device_id' });
 });
 
-test('test', async () => {
-  const e: Record<string, unknown> = {};
-  for (const [key, value] of Object.entries(e)) {
-    console.log(`${key}, ${value}`);
-  }
+test('editIdentity, setUserId setDeviceId, identity listener called', async () => {
+  const identityStore = new IdentityStoreImpl();
+  const expectedIdentity = { userId: 'user_id', deviceId: 'device_id' };
+  let listenerCalled = false;
+  identityStore.addIdentityListener((identity) => {
+    expect(identity).toEqual(expectedIdentity);
+    listenerCalled = true;
+  });
+  identityStore
+    .editIdentity()
+    .setUserId('user_id')
+    .setDeviceId('device_id')
+    .commit();
+  expect(listenerCalled).toEqual(true);
+});
+
+test('setIdentity, getIdentity, success', async () => {
+  const identityStore = new IdentityStoreImpl();
+  const expectedIdentity = { userId: 'user_id', deviceId: 'device_id' };
+  identityStore.setIdentity(expectedIdentity);
+  const identity = identityStore.getIdentity();
+  expect(identity).toEqual(expectedIdentity);
+});
+
+test('setIdentity, identity listener called', async () => {
+  const identityStore = new IdentityStoreImpl();
+  const expectedIdentity = { userId: 'user_id', deviceId: 'device_id' };
+  let listenerCalled = false;
+  identityStore.addIdentityListener((identity) => {
+    expect(identity).toEqual(expectedIdentity);
+    listenerCalled = true;
+  });
+  identityStore.setIdentity(expectedIdentity);
+  expect(listenerCalled).toEqual(true);
+});
+
+test('setIdentity with unchanged identity, identity listener not called', async () => {
+  const identityStore = new IdentityStoreImpl();
+  const expectedIdentity = { userId: 'user_id', deviceId: 'device_id' };
+  identityStore.setIdentity(expectedIdentity);
+  identityStore.addIdentityListener(() => {
+    fail('identity listener should not be called');
+  });
+  identityStore.setIdentity(expectedIdentity);
+});
+
+test('updateUserProperties, set', async () => {
+  const identityStore = new IdentityStoreImpl();
+  const identify = new amplitude.Identify()
+    .set('string', 'string')
+    .set('int', 32)
+    .set('bool', true)
+    .set('double', 4.2)
+    .set('jsonArray', [0, 1.1, true, 'three'])
+    .set('jsonObject', { key: 'value' });
+  identityStore
+    .editIdentity()
+    .updateUserProperties(identify['userPropertiesOperations'])
+    .commit();
+  const identity = identityStore.getIdentity();
+  expect(identity).toEqual({
+    userProperties: {
+      string: 'string',
+      int: 32,
+      bool: true,
+      double: 4.2,
+      jsonArray: [0, 1.1, true, 'three'],
+      jsonObject: { key: 'value' },
+    },
+  });
+});
+
+test('updateUserProperties, unset', async () => {
+  const identityStore = new IdentityStoreImpl();
+  identityStore.setIdentity({ userProperties: { key: 'value' } });
+  const identify = new amplitude.Identify().unset('key');
+  identityStore
+    .editIdentity()
+    .updateUserProperties(identify['userPropertiesOperations'])
+    .commit();
+  const identity = identityStore.getIdentity();
+  expect(identity).toEqual({ userProperties: {} });
+});
+
+test('updateUserProperties, setOnce', async () => {
+  const identityStore = new IdentityStoreImpl();
+  let identify = new amplitude.Identify().setOnce('key', 'value1');
+  identityStore
+    .editIdentity()
+    .updateUserProperties(identify['userPropertiesOperations'])
+    .commit();
+  let identity = identityStore.getIdentity();
+  expect(identity).toEqual({ userProperties: { key: 'value1' } });
+  identify = new amplitude.Identify().setOnce('key', 'value2');
+  identityStore
+    .editIdentity()
+    .updateUserProperties(identify['userPropertiesOperations'])
+    .commit();
+  identity = identityStore.getIdentity();
+  expect(identity).toEqual({ userProperties: { key: 'value1' } });
+});
+
+test('updateUserProperties, add to exiting number', async () => {
+  const identityStore = new IdentityStoreImpl();
+  const identify = new amplitude.Identify().set('int', 1).set('double', 1.1);
+  identityStore
+    .editIdentity()
+    .updateUserProperties(identify['userPropertiesOperations'])
+    .commit();
+  const add = new amplitude.Identify().add('int', 1.1).add('double', 1);
+  identityStore
+    .editIdentity()
+    .updateUserProperties(add['userPropertiesOperations'])
+    .commit();
+  const identity = identityStore.getIdentity();
+  expect(identity).toEqual({
+    userProperties: {
+      int: 2.1,
+      double: 2.1,
+    },
+  });
+});
+
+test('updateUserProperties, add to unset property', async () => {
+  const identityStore = new IdentityStoreImpl();
+  const add = new amplitude.Identify().add('int', 1).add('double', 1.1);
+  identityStore
+    .editIdentity()
+    .updateUserProperties(add['userPropertiesOperations'])
+    .commit();
+  const identity = identityStore.getIdentity();
+  expect(identity).toEqual({
+    userProperties: {
+      int: 1,
+      double: 1.1,
+    },
+  });
+});
+
+test('updateUserProperties, append existing array', async () => {
+  const identityStore = new IdentityStoreImpl();
+  identityStore.setIdentity({ userProperties: { key: [-3, -2, -1, 0] } });
+  const identify = new amplitude.Identify().append('key', [1, 2, 3]);
+  identityStore
+    .editIdentity()
+    .updateUserProperties(identify['userPropertiesOperations'])
+    .commit();
+  const identity = identityStore.getIdentity();
+  expect(identity).toEqual({
+    userProperties: {
+      key: [-3, -2, -1, 0, 1, 2, 3],
+    },
+  });
+});
+
+test('updateUserProperties, append to unset property', async () => {
+  const identityStore = new IdentityStoreImpl();
+  const identify = new amplitude.Identify().append('key', [1, 2, 3]);
+  identityStore
+    .editIdentity()
+    .updateUserProperties(identify['userPropertiesOperations'])
+    .commit();
+  const identity = identityStore.getIdentity();
+  expect(identity).toEqual({
+    userProperties: {
+      key: [1, 2, 3],
+    },
+  });
+});
+
+test('updateUserProperties, prepend to existing array', async () => {
+  const identityStore = new IdentityStoreImpl();
+  identityStore.setIdentity({ userProperties: { key: [0, 1, 2, 3] } });
+  const identify = new amplitude.Identify().prepend('key', [-3, -2, -1]);
+  identityStore
+    .editIdentity()
+    .updateUserProperties(identify['userPropertiesOperations'])
+    .commit();
+  const identity = identityStore.getIdentity();
+  expect(identity).toEqual({
+    userProperties: {
+      key: [-3, -2, -1, 0, 1, 2, 3],
+    },
+  });
+});
+
+test('updateUserProperties, prepend to unset array', async () => {
+  const identityStore = new IdentityStoreImpl();
+  const identify = new amplitude.Identify().prepend('key', [1, 2, 3]);
+  identityStore
+    .editIdentity()
+    .updateUserProperties(identify['userPropertiesOperations'])
+    .commit();
+  const identity = identityStore.getIdentity();
+  expect(identity).toEqual({
+    userProperties: {
+      key: [1, 2, 3],
+    },
+  });
 });
