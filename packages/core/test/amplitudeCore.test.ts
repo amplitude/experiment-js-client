2b7ba136680a248e0a9670b09a0881265f341ae7
Brian Giori <brian.giori@amplitude.com>
2021-11-29 15:53:08 -0800

    update test

--- packages/core/test/amplitudeCore.test.ts
+++ packages/core/test/amplitudeCore.test.ts
@@ -1,10 +1,10 @@
 import { getAmplitudeCore } from '../src/amplitudeCore';
 
 test('getAmplitudeCore returns the same instance', async () => {
-  const core = getAmpltudeCore('$default_instance');
+  const core = getAmplitudeCore('$default_instance');
   core.identityStore.setIdentity({ userId: 'userId' });
 
-  const core2 = getAmpltudeCore('$default_instance');
+  const core2 = getAmplitudeCore('$default_instance');
   const identity = core2.identityStore.getIdentity();
   expect(identity).toEqual({ userId: 'userId' });
 });
