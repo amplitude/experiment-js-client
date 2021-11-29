0f40f3c1124f9d71ca2cac48295902532d5f7d59
Brian Giori <brian.giori@amplitude.com>
2021-11-29 10:45:00 -0800

    remove provider consumer

--- packages/core/src/amplitudeCore.ts
+++ packages/core/src/amplitudeCore.ts
@@ -4,52 +4,9 @@ import { safeGlobal } from './util/global';
 
 safeGlobal['amplitudeCoreInstances'] = {};
 
-// export enum ComponentName {
-//   IdentityStore = 'IdentityStore',
-//   AnalyticsConnector = 'AnalyticsConnector',
-// }
-
-// type ComponentConsumer<T> = (component: T) => void;
-
 export class AmplitudeCore {
   public readonly identityStore = new IdentityStoreImpl();
   public readonly analyticsConnector = new AnalyticsConnectorImpl();
-
-  // private components: Record<string, unknown> = {};
-  // private consumers: Record<string, ComponentConsumer<unknown>[]> = {};
-
-  // constructor() {
-  //   this.produce(ComponentName.IdentityStore, new IdentityStoreImpl());
-  //   this.produce(
-  //     ComponentName.AnalyticsConnector,
-  //     new AnalyticsConnectorImpl(),
-  //   );
-  // }
-
-  // public consume<T>(name: ComponentName, callback: ComponentConsumer<T>): void {
-  //   const component = this.components[name];
-  //   if (component) {
-  //     callback(component as T);
-  //   } else {
-  //     if (!this.consumers[name]) {
-  //       this.consumers[name] = [];
-  //     }
-  //     this.consumers[name].push(callback);
-  //   }
-  // }
-
-  // public produce<T>(name: ComponentName, component: T): void {
-  //   if (this.components[name]) {
-  //     return;
-  //   }
-  //   this.components[name] = component;
-  //   const consumers = this.consumers[name];
-  //   if (consumers) {
-  //     for (const consumer of Object.values(consumers)) {
-  //       consumer(component as T);
-  //     }
-  //   }
-  // }
 }
 
 export const getAmpltudeCore = (instanceName: string): AmplitudeCore => {
