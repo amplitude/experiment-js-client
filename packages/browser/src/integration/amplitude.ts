5fcf4615677a280b4acd5edfd6ce81ec1c14f6eb
Brian Giori <briangiori@gmail.com>
2022-01-20 12:01:08 -0800

    fix: type issue with global amp instance (#18)

--- packages/browser/src/integration/amplitude.ts
+++ packages/browser/src/integration/amplitude.ts
@@ -6,11 +6,6 @@ import {
 import { ExperimentUser } from '../types/user';
 import { safeGlobal } from '../util/global';
 
-declare global {
-  // eslint-disable-next-line no-var, @typescript-eslint/no-explicit-any
-  var amplitude: any;
-}
-
 type AmplitudeIdentify = {
   set(property: string, value: unknown): void;
   unset(property: string): void;
@@ -101,9 +96,13 @@ export class AmplitudeAnalyticsProvider implements ExperimentAnalyticsProvider {
   }
 
   unsetUserProperty(event: ExperimentAnalyticsEvent): void {
+    const amplitude = safeGlobal['amplitude'];
+    if (!amplitude) {
+      return;
+    }
     // if the variant does not have a value, unset the user property
     this.amplitudeInstance.identify(
-      new safeGlobal.amplitude.Identify().unset(event.userProperty),
+      new amplitude.Identify().unset(event.userProperty),
     );
   }
 }
