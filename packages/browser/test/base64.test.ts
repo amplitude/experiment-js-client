a5884be608235702767d4582b2edeac7ac0ae79a
Brian Giori <brian.giori@amplitude.com>
2022-01-26 14:22:58 -0800

    fix lint

--- packages/browser/test/base64.test.ts
+++ packages/browser/test/base64.test.ts
@@ -2,29 +2,8 @@ import { stringToUtf8Array, urlSafeBase64Encode } from '../src/util/base64';
 
 test('stringToUtf8Array', () => {
   expect(stringToUtf8Array('My 🚀 is full of 🦎')).toEqual([
-    77,
-    121,
-    32,
-    240,
-    159,
-    154,
-    128,
-    32,
-    105,
-    115,
-    32,
-    102,
-    117,
-    108,
-    108,
-    32,
-    111,
-    102,
-    32,
-    240,
-    159,
-    166,
-    142,
+    77, 121, 32, 240, 159, 154, 128, 32, 105, 115, 32, 102, 117, 108, 108, 32,
+    111, 102, 32, 240, 159, 166, 142,
   ]);
 });
 
