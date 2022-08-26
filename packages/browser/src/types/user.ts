24282fc7e15164b28a216a672e079c6eb3aa21ff
Brian Giori <brian.giori@amplitude.com>
2022-08-25 22:20:24 -0700

    support groups and group properties on skylab user

--- packages/browser/src/types/user.ts
+++ packages/browser/src/types/user.ts
@@ -86,4 +86,20 @@ export type ExperimentUser = {
       | boolean
       | Array<string | number | boolean>;
   };
+
+  groups?: {
+    [groupType: string]: string[];
+  };
+
+  group_properties?: {
+    [groupType: string]: {
+      [groupName: string]: {
+        [propertyName: string]:
+          | string
+          | number
+          | boolean
+          | Array<string | number | boolean>;
+      };
+    };
+  };
 };
