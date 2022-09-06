f8eec48a104ed8e2e34b823279b4e8100d290c46
Brian Giori <brian.giori@amplitude.com>
2022-09-06 15:50:21 -0700

    fix: move isEqual to util, add null check (#42)

---
+++ packages/analyticsConnector/test/equals.test.ts
@@ -0,0 +1,23 @@
+import { isEqual } from 'src/util/equals';
+
+describe('isEqual', () => {
+  test('isEqual, one null on non-null, is false', () => {
+    const actual = isEqual('non-null', null);
+    expect(actual).toEqual(false);
+  });
+
+  test('isEqual, two null, is true', () => {
+    const actual = isEqual(null, null);
+    expect(actual).toEqual(true);
+  });
+
+  test('isEqual, two non-null equals, is true', () => {
+    const actual = isEqual('non-null', 'non-null');
+    expect(actual).toEqual(true);
+  });
+
+  test('isEqual, user objects with null user ids, is true', () => {
+    const actual = isEqual({ user_id: null }, { user_id: null });
+    expect(actual).toEqual(true);
+  });
+});
