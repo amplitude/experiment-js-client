97cc3a0371dd88fd8c58c75cf550891ce6ab0ceb
Brian Giori <brian.giori@amplitude.com>
2023-08-28 14:04:31 -0700

    use safeglobal with local storage

--- packages/experiment-browser/src/storage/local-storage.ts
+++ packages/experiment-browser/src/storage/local-storage.ts
@@ -1,14 +1,16 @@
+import { safeGlobal } from '@amplitude/experiment-core';
+
 import { Storage } from '../types/storage';
 export class LocalStorage implements Storage {
   async get(key: string): Promise<string> {
-    return localStorage.getItem(key);
+    return safeGlobal.localStorage.getItem(key);
   }
 
   async put(key: string, value: string): Promise<void> {
-    localStorage.setItem(key, value);
+    safeGlobal.localStorage.setItem(key, value);
   }
 
   async delete(key: string): Promise<void> {
-    localStorage.removeItem(key);
+    safeGlobal.localStorage.removeItem(key);
   }
 }
