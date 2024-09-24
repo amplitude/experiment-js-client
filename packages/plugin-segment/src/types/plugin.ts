5fe1b0954bb72d8fa7a31d02b663b853b2681f6e
Brian Giori <brian.giori@amplitude.com>
2024-09-23 17:06:19 -0700

    chore: add comment

--- packages/plugin-segment/src/types/plugin.ts
+++ packages/plugin-segment/src/types/plugin.ts
@@ -2,8 +2,21 @@ import { IntegrationPlugin } from '@amplitude/experiment-js-client';
 import { Analytics } from '@segment/analytics-next';
 
 export interface Options {
+  /**
+   * An existing segment analytics instance. This instance will be used instead
+   * of the instance on the window defined by the instanceKey.
+   */
   instance?: Analytics;
+  /**
+   * The key of the field on the window that holds the segment analytics
+   * instance when the script is loaded via the script loader.
+   *
+   * Defaults to "analytics".
+   */
   instanceKey?: string;
+  /**
+   * Skip waiting for the segment SDK to load and be ready.
+   */
   skipSetup?: boolean;
 }
 
