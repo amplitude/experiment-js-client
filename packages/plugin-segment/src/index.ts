4d4afd4fb51cb88fdc3a87b9a587af9c37e59dca
Brian Giori <brian.giori@amplitude.com>
2024-10-09 13:53:04 -0700

    chore: add experiment segment plugin (#128)

---
+++ packages/plugin-segment/src/index.ts
@@ -0,0 +1,3 @@
+export { segmentIntegrationPlugin } from './plugin';
+export { segmentIntegrationPlugin as plugin } from './plugin';
+export { SegmentIntegrationPlugin, Options } from './types/plugin';
