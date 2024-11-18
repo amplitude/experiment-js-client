a93d63a638fc529da9159537aee5859a0ff0816f
tyiuhc <tim.yiu@amplitude.com>
2024-11-18 15:17:24 -0800

    fix tests

--- packages/experiment-tag/test/experiment.test.ts
+++ packages/experiment-tag/test/experiment.test.ts
@@ -23,9 +23,11 @@ jest.spyOn(experiment, 'setUrlChangeListener').mockReturnValue(undefined);
 
 describe('initializeExperiment', () => {
   const mockGetGlobalScope = jest.spyOn(experimentCore, 'getGlobalScope');
+  const mockExposure = jest.spyOn(ExperimentClient.prototype, 'exposure');
+  const mockUser = { user_id: 'user_id', device_id: 'device_id' };
   jest.spyOn(ExperimentClient.prototype, 'setUser');
   jest.spyOn(ExperimentClient.prototype, 'all');
-  const mockExposure = jest.spyOn(ExperimentClient.prototype, 'exposure');
+  jest.spyOn(ExperimentClient.prototype, 'getUser').mockReturnValue(mockUser);
   jest.spyOn(util, 'UUID').mockReturnValue('mock');
   let mockGlobal;
 
@@ -356,9 +358,6 @@ describe('initializeExperiment', () => {
   });
 
   test('remote evaluation - request web remote flags', () => {
-    const mockUser = { user_id: 'user_id', device_id: 'device_id' };
-    jest.spyOn(ExperimentClient.prototype, 'getUser').mockReturnValue(mockUser);
-
     const initialFlags = [
       // remote flag
       createMutateFlag('test-2', 'treatment', [], [], [], 'remote'),
