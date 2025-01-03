22317920d1a5734029ebdc0dd2056ec1303aaa2a
tyiuhc <tim.yiu@amplitude.com>
2025-01-03 11:25:38 -0800

    add server zone config

--- packages/experiment-tag/src/script.ts
+++ packages/experiment-tag/src/script.ts
@@ -2,7 +2,11 @@ import { initializeExperiment } from './experiment';
 
 const API_KEY = '{{DEPLOYMENT_KEY}}';
 const initialFlags = '{{INITIAL_FLAGS}}';
-initializeExperiment(API_KEY, initialFlags).then(() => {
-  // Remove anti-flicker css if it exists
-  document.getElementById('amp-exp-css')?.remove();
-});
+const serverZone = '{{SERVER_ZONE}}';
+
+initializeExperiment(API_KEY, initialFlags, { serverZone: serverZone }).then(
+  () => {
+    // Remove anti-flicker css if it exists
+    document.getElementById('amp-exp-css')?.remove();
+  },
+);
