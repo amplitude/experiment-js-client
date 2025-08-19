659a2fe1db4db288cbb2ef2e7317d3f74d6ec0ae
Tim Yiu <137842098+tyiuhc@users.noreply.github.com>
2025-08-19 15:40:59 -0700

    remove preview modal

--- packages/experiment-tag/src/experiment.ts
+++ packages/experiment-tag/src/experiment.ts
@@ -16,7 +16,6 @@ import * as FeatureExperiment from '@amplitude/experiment-js-client';
 import mutate, { MutationController } from 'dom-mutator';
 
 import { MessageBus } from './message-bus';
-import { showPreviewModeModal } from './preview/preview';
 import { PageChangeEvent, SubscriptionManager } from './subscriptions';
 import { Defaults, WebExperimentClient, WebExperimentConfig } from './types';
 import {
@@ -172,10 +171,6 @@ export class DefaultWebExperimentClient implements WebExperimentClient {
           ),
         );
       }
-
-      showPreviewModeModal({
-        flags: this.previewFlags,
-      });
     }
 
     const initialFlagsString = JSON.stringify(this.initialFlags);
