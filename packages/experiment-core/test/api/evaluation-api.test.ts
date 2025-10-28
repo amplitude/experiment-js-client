5821c649a6bc41a45c51567b9be9f7addbe7647d
Peter Zhu <peter.zhu@amplitude.com>
2025-10-28 10:08:20 -0700

    fix: lint

--- packages/experiment-core/test/api/evaluation-api.test.ts
+++ packages/experiment-core/test/api/evaluation-api.test.ts
@@ -1,8 +1,9 @@
-import { SdkEvaluationApi } from "../../src";
-import { Base64 } from "js-base64";
+import { Base64 } from 'js-base64';
+
+import { SdkEvaluationApi } from '../../src';
 
 const VARIANTS = {
-  "flag-1": { key: 'on', value: 'on' },
+  'flag-1': { key: 'on', value: 'on' },
 };
 const USER = { user_id: 'test-user' };
 
@@ -18,8 +19,12 @@ const getMockHttpClient = () => {
 describe('Evaluation API', () => {
   it('should get variants', async () => {
     const mockHttpClient = getMockHttpClient();
-    const evaluationApi = new SdkEvaluationApi('test-deployment-key', 'https://server.url.amplitude.com', mockHttpClient);
-    
+    const evaluationApi = new SdkEvaluationApi(
+      'test-deployment-key',
+      'https://server.url.amplitude.com',
+      mockHttpClient,
+    );
+
     const variants = await evaluationApi.getVariants(USER);
 
     expect(variants).toEqual(VARIANTS);
@@ -35,8 +40,12 @@ describe('Evaluation API', () => {
 
   it('should get variants with options', async () => {
     const mockHttpClient = getMockHttpClient();
-    const evaluationApi = new SdkEvaluationApi('test-deployment-key', 'https://server.url.amplitude.com', mockHttpClient);
-    
+    const evaluationApi = new SdkEvaluationApi(
+      'test-deployment-key',
+      'https://server.url.amplitude.com',
+      mockHttpClient,
+    );
+
     const variants = await evaluationApi.getVariants(USER, {
       flagKeys: ['flag-1'],
       trackingOption: 'no-track',
@@ -48,7 +57,8 @@ describe('Evaluation API', () => {
 
     expect(variants).toEqual(VARIANTS);
     expect(mockHttpClient.request).toHaveBeenCalledWith({
-      requestUrl: 'https://server.url.amplitude.com/sdk/v2/vardata?v=0&eval_mode=local&delivery_method=web',
+      requestUrl:
+        'https://server.url.amplitude.com/sdk/v2/vardata?v=0&eval_mode=local&delivery_method=web',
       method: 'GET',
       headers: {
         Authorization: 'Api-Key test-deployment-key',
