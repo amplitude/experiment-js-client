34da31704662817c4cf2fae3e4aa6f564bc13117
Tim Yiu <137842098+tyiuhc@users.noreply.github.com>
2026-03-06 10:38:55 -0800

    feat: Behavioral Targeting - EventStorage and BehavioralTargetingEvaluator

---
+++ packages/experiment-tag/src/behavioral-targeting/index.ts
@@ -0,0 +1,8 @@
+export { SessionManager } from './session-manager';
+export { EventStorageManager, EventRecord } from './event-storage';
+export { BehavioralTargetingEvaluator } from './evaluator';
+export {
+  BehavioralTargeting,
+  BehavioralCondition,
+  BehavioralConditionSet,
+} from './types';
