dcfad3a500c2dfc8d71f0d5d363749207db7fdda
Kevin Liou <kevin.liou@amplitude.com>
2026-06-22 17:36:06 -0700

    Implement patchDOMParser for style CSP workaround
    Add patchDOMParser function to handle inline styles.

--- packages/experiment-tag/src/util/patch.ts
+++ packages/experiment-tag/src/util/patch.ts
@@ -10,3 +10,28 @@ export const patchRemoveChild = () => {
     return n;
   };
 };
+
+/**
+ * Patch DOMParser to set inline styles programmatically to work around restrictive style CSP
+ */
+export const patchDOMParser = () => {
+  if (!window['__domParserParseFromString']) {
+    const parseFromString = DOMParser.prototype.parseFromString;
+    window['__domParserParseFromString'] = parseFromString;
+  
+    const setStyle = (el: HTMLElement) => {
+      el.style.cssText = el.getAttribute('style') || '';
+      for (const child of el.children) {
+        setStyle(child as HTMLElement);
+      }
+    };
+  
+    DOMParser.prototype.parseFromString = function (content, contentType) {
+      const doc = parseFromString.apply(this, [content, contentType]);
+      if (contentType === 'text/html') {
+        setStyle(doc.body);
+      }
+      return doc;
+    };
+  }
+}
