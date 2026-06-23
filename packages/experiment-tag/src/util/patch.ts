0d832c6ecf218770df59e04a9c34de8359f53524
Kevin Liou <kevin.liou@amplitude.com>
2026-06-22 18:01:04 -0700

    add detection

--- packages/experiment-tag/src/util/patch.ts
+++ packages/experiment-tag/src/util/patch.ts
@@ -11,20 +11,28 @@ export const patchRemoveChild = () => {
   };
 };
 
+const detectStyleCSP = () => {
+  const el = new DOMParser().parseFromString(
+    '<i style="color:red"></i>',
+    'text/html',
+  ).body.firstChild as HTMLElement;
+  return !el.style.cssText;
+};
+
+const setStyle = (el: HTMLElement) => {
+  el.style.cssText = el.getAttribute('style') || '';
+  for (const child of el.children) {
+    setStyle(child as HTMLElement);
+  }
+};
+
 /**
  * Patch DOMParser to set inline styles programmatically to work around restrictive style CSP
  */
 export const patchDOMParser = () => {
-  if (!window['__domParserParseFromString']) {
+  if (!window['__domParserParseFromString'] && detectStyleCSP()) {
     const parseFromString = DOMParser.prototype.parseFromString;
     window['__domParserParseFromString'] = parseFromString;
-  
-    const setStyle = (el: HTMLElement) => {
-      el.style.cssText = el.getAttribute('style') || '';
-      for (const child of el.children) {
-        setStyle(child as HTMLElement);
-      }
-    };
   
     DOMParser.prototype.parseFromString = function (content, contentType) {
       const doc = parseFromString.apply(this, [content, contentType]);
